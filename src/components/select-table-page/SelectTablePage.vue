cdc5e4687dcaf4a00d5baf5bff5954eafda150c2
pp_chjz <pirapat.j@ku.th>
2023-02-16 22:15:08 +0700

    select table page

---
+++ src/components/select-table-page/SelectTablePage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="about">
+    <h1>This is an select page</h1>
+    <c-box >
+        <c-select v-model="table_number" placeholder="Select table" size="md" mt="13%" borderColor="gray.800">
+                <option v-for="index in table" :value="1"> {{ index }}</option>
+        </c-select>
+    </c-box>
+  </div>
+</template>
+
+<script>
+import { CSelect , CReset , CBox } from "@chakra-ui/vue";
+import HelloWorld from '@/components/HelloWorld.vue'
+import NavBar from '@/components/NavBar.vue'
+
+
+export default {
+  name: "App",
+  components: {
+    CSelect,
+    CReset,
+    CBox,
+    HelloWorld,
+    NavBar,
+  },
+  data(){
+      return{
+          table_number:"",
+          table:['1','2','3','4']
+      }
+  },
+  async created(){
+      console.log("select table page created");
+  },
+  methods:{
+
+  }
+};
+</script>
